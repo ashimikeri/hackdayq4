952be5872866490c48329c984eadd32cc2413863
ashimikeri <43859120+ashimikeri@users.noreply.github.com>
2018-10-04 14:14:07 -0700

    Update server.js
    CVE JSON formatted data

--- server.js
+++ server.js
@@ -23,6 +23,20 @@ app.post('/devicelayer', (req, res) => {
   res.send('Working\n');
 });
 
-app.listen(PORT, HOST);
+// app.listen(PORT, HOST);
 
-console.log(`Running on http://${HOST}:${PORT}`);
+// console.log(`Running on http://${HOST}:${PORT}`);
+function resp_constructor (cve_data) 
+{
+  var cve_input = "["
+  for (var i=0;i<cve_data.length;i++)
+{
+   cve_input += '{"id" : '+cve_data[i][0]+', "rating" : '+cve_data[i][1]+', "summary" : '+cve_data[i][2]+'},';
+}
+  var regex = /},$/g;
+  cve_input.replace(regex, '}');
+  cve_input += ']';
+  return JSON.parse(cve_input) ;
+}
+
+     
